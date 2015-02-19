f55ede0e59b38cabfd99b3edb60c7d4bb5ef1690
Robin de Mourat <robin.demourat@gmail.com>
2015-02-19 14:32:39 +0100

    set interface skeleton

--- app/scripts/controllers/layout.js
+++ app/scripts/controllers/layout.js
@@ -9,9 +9,64 @@
  */
 angular.module('garderlefilApp')
   .controller('LayoutCtrl', function ($scope, $location) {
+
   	$scope.menuOut = true;
+  	$scope.pagesList = [];
+
+  	//getting the pages list for scrolling - I want it to be easily changeable so I used jquery and base on html rather than some data
+  	$('.menu').find('a').each(function(i, el){
+  		$scope.pagesList.push($(el).attr('href'));
+  	});
 
   	$scope.$on('$routeChangeStart', function(next, current) { 
   		$scope.menuOut = true;
  	});
+
+ 	$scope.setViewClass = function(){
+ 		var out = "";
+
+ 		if($scope.menuOut)
+ 			out += 'col-sm-offset-2 col-sm-10 col-xs-offset-4 col-xs-8';
+ 		else
+ 			out += 'col-sm-offset-1 col-sm-11 col-xs-offset-1 col-xs-11';
+ 		out += " " + $scope.animationClass;
+
+ 		return out;
+ 	}
+
+ 	$scope.$on('upPage', function(){
+ 		$scope.animationClass = "slide-in-top slide-out-bottom";
+ 	});
+
+ 	$scope.$on('downPage', function(){
+ 		$scope.animationClass = "slide-in-bottom slide-out-top";
+ 	});
+
+ 	$scope.$on('noPage', function(){
+ 		$scope.animationClass = "slide-in-simple slide-out-simple";
+ 	});
+
+ 	//navigation
+ 	$scope.$on('$routeChangeSuccess', function(event, current, previous){
+			var index = $scope.getPageIndex();
+
+			if(index < $scope.pagesList.length - 2)
+ 			 $scope.downUrl = $scope.pagesList[+index+1];
+ 			else $scope.downUrl = undefined;
+
+ 			if(index > 1)
+ 			 $scope.upUrl = $scope.pagesList[+index-1];
+ 			else $scope.upUrl = undefined;
+ 	});
+
+ 	$scope.getPageIndex = function(){
+ 		//getting the current index in playlist
+ 		var index;
+ 		for(var i in $scope.pagesList){
+ 			if($scope.pagesList[i] === '#'+$location.path())
+ 				index = i;
+ 		}
+ 		return index;
+ 	};
+
   });
--- app/scripts/directives/navmen.js
+++ app/scripts/directives/navmen.js
@@ -14,19 +14,45 @@ angular.module('garderlefilApp')
         var menuItems= $(element).find('.menu-item');
         var subMenus = $(element).find('.submenu');
         
-        scope.$on("$routeChangeSuccess", function (event, current, previous) {
-        	if(!current.$$route)
+        scope.$on("$routeChangeStart", function (event, current, previous) {
+        	if(!current || !current.$$route)
         	 current = previous;
+        	else if(!previous || !previous.$$route)
+        	 previous = current;
 
 		    var curPath = '#' + (current.$$route.originalPath);
-		    //add active to active path
+		    var prevPath = '#' + (previous.$$route.originalPath);
+		    //add active to active path and determine if up or down
 		    angular.forEach(menuItems, function(value, key){
+		    	i++;
 		    	var el = angular.element(value);
-		    	if($(el).find('a').attr('href') === curPath)
+		    	if($(el).find('a').attr('href') === curPath){
+		    		curI = i;
 		    		el.addClass('active');
-		    	else if(el.hasClass('active'))
+		    	}else if(el.hasClass('active'))
 		    		el.removeClass('active');
+		    	if($(el).find('a').attr('href') === prevPath){
+		    		prevI = i;
+		    	};
 		    });
+
+		    //get if up or down
+		    var i = 0, curI, prevI;
+		    angular.forEach($(element).find('a'), function(value, key){
+		    	i++;
+		    	var el = angular.element(value);
+		    	if($(el).attr('href') === curPath)
+		    		curI = i;
+		    	else if($(el).attr('href') === prevPath)
+		    		prevI = i;
+		    });
+
+		    if(curI > prevI){
+		    	scope.$emit('downPage');
+		    }else if(curI < prevI){
+		    	scope.$emit('upPage');
+		    }
+		    
 		    //open or close concerned submenu items
 		    angular.forEach(subMenus, function(value, key){
 		    	var el = angular.element(value);
