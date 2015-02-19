78551f1e204642bafa362436a5530ba0456b56ee
Robin de Mourat <robin.demourat@gmail.com>
2015-02-19 22:45:13 +0100

    fitwidth

--- app/scripts/directives/fitwidth.js
+++ app/scripts/directives/fitwidth.js
@@ -13,29 +13,6 @@ angular.module('garderlefilApp')
       link: function postLink(scope, element, attrs, $window) {
 
         var w = angular.element($window);
-       /* scope.getWindowDimensions = function () {
-            return {
-                'h': w.height(),
-                'w': w.width()
-            };
-        };
-        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
-            scope.windowHeight = newValue.h;
-            scope.windowWidth = newValue.w;
-
-            scope.style = function () {
-                return {
-                    'height': (newValue.h - 100) + 'px',
-                        'width': (newValue.w - 100) + 'px'
-                };
-            };
-
-        }, true);*/
-
-        /*w.bind('resize', function () {
-          console.log("resize");
-            size();
-        });*/
         window.onResize = size;
 
         var size = function(){
